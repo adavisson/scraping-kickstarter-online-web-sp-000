bc7ec9be65473a01d12e3627289b69459c7f595a
Andrew Davisson <akdavisson4@gmail.com>
2019-06-14 22:25:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
   #kickstarter.css("li.project.grid_4")
-  #title: 
+  #title: project.css("h2.bbcard_name strong a").text
 
   binding.pry  
   
